3f3da76cf2e71b5a4bf75b1eea3e3307f3c9fd35
thisaldil <tdimith10@gmail.com>
2025-01-30 10:14:16 +0530

    profile navigates

--- client/app/app.tsx
+++ client/app/app.tsx
@@ -1,10 +1,14 @@
 // App.tsx
 
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Index from './index';  // Your Splash screen
-import Login from './index';  // Your index screen
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Index from "./index"; // Your Splash screen
+import Login from "./index"; // Your index screen
+import AccountSecurity from "./profileComponents/AccountSecurity";
+import WorkInformation from "./profileComponents/WorkInformation";
+import PersonalDetails from "./profileComponents/PersonalDetails";
+import Resume from "./profileComponents/Resume";
 
 const Stack = createStackNavigator();
 
@@ -17,10 +21,11 @@ const App = () => {
           component={Index}
           options={{ headerShown: false }} // Hide the header for the splash screen
         />
-        <Stack.Screen
-          name="Login"
-          component={Login}
-        />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="AccountSecurity" component={AccountSecurity} />
+        <Stack.Screen name="WorkInformation" component={WorkInformation} />
+        <Stack.Screen name="PersonalDetails" component={PersonalDetails} />
+        <Stack.Screen name="Resume" component={Resume} />
       </Stack.Navigator>
     </NavigationContainer>
   );
