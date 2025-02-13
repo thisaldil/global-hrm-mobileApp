79e23bdf9e0492e8bc7dfd2ee8c35a1c2a83ef78
sandeepa mallawarachchi <126542051+sandeepaMallawarachchi@users.noreply.github.com>
2025-02-13 10:35:49 +0530

    Update firebase.js

--- client/firebase/firebase.js
+++ client/firebase/firebase.js
@@ -4,13 +4,13 @@ import { getAuth, signInAnonymously } from "firebase/auth";
 
 // Firebase configuration
 export const firebaseConfig = {
-  apiKey: "AIzaSyBUWjq5TEZ7lh9JpzMCfGLOPX_WbLqBLGw",
-  authDomain: "hrm-system-9f4a3.firebaseapp.com",
-  databaseURL: "https://hrm-system-9f4a3-default-rtdb.firebaseio.com",
-  projectId: "hrm-system-9f4a3",
-  storageBucket: "hrm-system-9f4a3.appspot.com",
-  messagingSenderId: "698601974813",
-  appId: "1:698601974813:web:8331ea64861511b33eeaf4",
+  apiKey: "apiKey",
+  authDomain: "authDomain",
+  databaseURL: "databaseURL",
+  projectId: "projectId",
+  storageBucket: "storageBucket",
+  messagingSenderId: "messagingSenderId",
+  appId: "appId",
 };
 
 // Initialize Firebase
